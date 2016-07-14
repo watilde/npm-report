6ed505d90eb967e5565f38d3cf694a09cfbf5de7
Daijiro Wachi <daijiro.wachi@gmail.com>
2016-07-14 15:59:26 +0200

    Update test cases

--- test/test.js
+++ test/test.js
@@ -1,12 +1,6 @@
-var fs = require("fs")
-var path = require("path")
+var fs = require('fs')
+var path = require('path')
 var test = require("tap").test
-var createURL = require("../lib/create-url")
-var fixturePath = path.join(__dirname, "fixture.txt")
-var fixture =  fs.readFileSync(fixturePath, {encoding: "utf8"})
-
-test("create url test", function (t) {
-  var url = createURL("title")
-  t.equal(fixture, url)
+test('', function (t) {
   t.end()
 })
